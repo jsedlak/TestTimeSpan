b481295959881eaece4bbc4aa8cf7d8a56e9b6ae
John Sedlak <john.sedlak@outlook.com>
2020-09-16 09:39:59 -0400

    Initial

---
+++ TestTimeSpan/Shared/WeatherForecast.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestTimeSpan.Shared
+{
+    public class WeatherForecast
+    {
+        public TimeSpan ForecastLength { get; set; } = TimeSpan.FromMinutes(60);
+
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
